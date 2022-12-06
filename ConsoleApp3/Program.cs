1aaa73cc8db2f548134f409d25eacc24e4e5147d
Lu4og <lu4lu4.777@gmail.com>
2022-12-06 12:15:49 +0300

    новый ноут

---
+++ ConsoleApp3/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace pract6
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Путь до файла который надо открыть: ");
+            string firstPath = Console.ReadLine();
+            string text = Editor.Edit(Converter.GenerateString(firstPath));
+            File.WriteAllText(firstPath, text);
+            Console.Clear();
+            Console.Write("Путь до файла в который надо сохранить: ");
+            string secondPath = Console.ReadLine();
+            Converter.GenerateFile(firstPath, secondPath);
+            Console.WriteLine("Тут мои полномочия все");
+        }
+    }
+} 
